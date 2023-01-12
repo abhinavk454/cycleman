cd75d24a4f60cc386fb2886a5f9a0baa07f925e8
Abhinav Kumar <abhinav.vikash2@gmail.com>
2023-01-12 23:08:03 +0530

    Made Production Ready Version 0.0.3

--- CycleMan/settings.py
+++ CycleMan/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "CycleManApp",
-    "rest_framework",
-    "debug_toolbar"
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
@@ -49,8 +48,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware"
+    "django.middleware.clickjacking.XFrameOptionsMiddleware"
 ]
 
 ROOT_URLCONF = "CycleMan.urls"
--- CycleMan/urls.py
+++ CycleMan/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("CycleManApp.urls")),
-    path("__debug__/", include("debug_toolbar.urls"))
 ]
