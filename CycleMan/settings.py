72610610f0878cfe581cab951e2b525ff4ab6964
Abhinav Kumar <abhinav.vikash2@gmail.com>
2023-01-12 22:54:13 +0530

    Made Production Ready Version 0.0.2

--- CycleMan/settings.py
+++ CycleMan/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ht$#rlyk*e@^a)tif1nau9tnbv5gx%tb&40)pvk9ea0!rxtv!w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
